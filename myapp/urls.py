2ae0afa205e397713eb89948da3454f5b3baea7d
sunwise <sunwise9634@gmail.com>
2022-06-19 12:15:44 +0900

    read 기능 추가

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,6 @@ from myapp import views
 
 urlpatterns = [
     path('', views.index),
-    path('create/', views.index),
-    path('read/1/', views.index)
+    path('create/', views.create),
+    path('read/<id>/', views.read) # id 값을 인자로 받을수 있음
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,52 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
-def index(request):
 
-    # http 로 응답을 하겠다
-    return HttpResponse('Welcome!')
+topics = [
+    {'id':1, 'title':'routing', 'body':'Routing is ...'},
+    {'id':2, 'title':'view', 'body':'View is ...'},
+    {'id':3, 'title':'model', 'body':'Model is ...'}
+]
+
+def HTMLTemplate(articleTag):
+    global topics
+
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    
+    return f'''
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            {ol}
+        </ol>
+        {articleTag}
+    </body>
+    </html>
+    '''
+
+
+def index(request):
+    article = '''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+
+    return HttpResponse(HTMLTemplate(article))
+
+
+def create(request):
+
+    return HttpResponse('Create!')
+
+
+def read(request, id):
+    global topics
+
+    article = ''
+    for topic in topics:
+        if topic["id"] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+
+    return HttpResponse(HTMLTemplate(article))
